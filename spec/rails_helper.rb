27cde4183de19c633ecce5abd036fb7c1fc73c26
nlabrazi <na.labrazi@gmail.com>
2026-05-05 17:11:27 +0200

    refactor: Refactoring: Use environment variables for database configuration
    - Improved readability and maintainability of the code by using environment variables for database configuration.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # spec/rails_helper.rb
 require 'webmock/rspec'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 ENV['GITHUB_TOKEN'] ||= 'test-token'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
